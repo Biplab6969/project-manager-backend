ec79b1bd68db8ae2aabbe704caecc04d3c7d6edb
Biplab jena <jbiplab80@gmail.com>
2025-09-03 19:34:40 +0530

    hhi

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,5 +1,5 @@
 import User from "../models/user.js";
-import bcrypt from "bcryptjs";
+import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import Verification from "../models/verification.js";
 import { sendEmail } from "../libs/send-email.js";
